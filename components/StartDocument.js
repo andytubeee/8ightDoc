de9200d9b60e838623fed1438b40999d0a1c3d1c
Andrew Yang <andy.tubeee@gmail.com>
2021-08-27 22:45:36 -0400

    Create new document modal

--- components/StartDocument.js
+++ components/StartDocument.js
@@ -2,7 +2,33 @@ import Button from '@material-tailwind/react/Button';
 import Icon from '@material-tailwind/react/Icon';
 import Image from 'next/image';
 import PlusIcon from '../public/assets/icons8-plus-+.svg';
+import Swal from 'sweetalert2';
+
 const StartDocument = () => {
+  const createNewDocumentPopup = () => {
+    Swal.fire({
+      title: 'Create a new document',
+      input: 'text',
+      inputAttributes: {
+        placeholder: 'Document Name',
+      },
+      showCancelButton: true,
+      confirmButtonText: 'Create',
+      showLoaderOnConfirm: true,
+    }).then((res) => {
+      if (res.isConfirmed) {
+        if (res.value.length <= 0)
+          return Swal.fire('Error', 'Invalid document name', 'error');
+
+        Swal.fire('Success', 'New document created', 'success').then((_) => {
+          createNewDocument(res.value);
+        });
+      }
+    });
+  };
+  const createNewDocument = (title) => {
+    console.log(title);
+  };
   return (
     <section className='bg-green-50 p-2'>
       <div className='max-w-3xl mx-auto'>
@@ -20,7 +46,10 @@ const StartDocument = () => {
           </Button>
         </div>
         <div className=''>
-          <div className='flex items-center justify-center h-52 w-40 bg-white rounded-md cursor-pointer group mb-2 border-2 hover:border-green-300 transform transition-all duration-1000'>
+          <div
+            className='flex items-center justify-center h-52 w-40 bg-white rounded-md cursor-pointer group mb-2 border-2 hover:border-green-300 transform transition-all duration-1000'
+            onClick={createNewDocumentPopup}
+          >
             {' '}
             <Image src={PlusIcon} width={40} height={40} />
           </div>
